ffd60d924bb499b88098a7d87fb1af3cc1b6fcb1
Andrii <myemail@gmail.com>
2023-12-26 19:20:58 +0200

    Lab4

---
+++ lab4/Task.Core/TaskManagement.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3TaskManager
+{
+    public class TaskManagement
+    {
+        public List<Task> Tasks { get; }
+        public void ShowTask(int index)
+        {
+            if (index >= 0 && index < Tasks.Count)
+            {
+                Console.WriteLine($"Task Index {index}\n");
+                Console.WriteLine($"Title {Tasks[index].Title}\n");
+                Console.WriteLine($"Description {Tasks[index].Description}\n");
+                Console.WriteLine($"Priority {Tasks[index].Priority}\n");
+                Console.WriteLine($"Favorite {Tasks[index].Favorite}\n");
+                Console.WriteLine("\n \n \n");
+            }
+            else
+                Console.WriteLine($"Wrong Task Index {index}\n");
+
+        }
+        public TaskManagement() { Tasks = new List<Task>(); }
+        public bool CheckPriority(string priority)
+        {
+            switch (priority)
+            {
+                case null:
+                case "Lowest":
+                case "Low":
+                case "Medium":
+                case "High":
+                case "Highest":
+                    return true;
+                default: return false;
+            }
+        }
+        public bool IsValid(Task task)
+        {
+            if (string.IsNullOrWhiteSpace(task.Title) || string.IsNullOrWhiteSpace(task.Description) || !CheckPriority(task.Priority)) return false; return true;
+        }
+        private bool TaskExist(string title) { return Tasks.Where(x => x.Title == title).Any(); }
+
+
+
+        public void AddTaskPriority(string task, string priority)
+        {
+            if (!TaskExist(task))
+                throw new InvalidOperationException($"Task {task} doesn`t exist. ");
+            if (!CheckPriority(priority))
+                throw new InvalidOperationException($"Wrong Priority {priority} for Task {task}. ");
+
+            Tasks.First(x => x.Title == task).Priority = priority;
+        }
+        public void RemoveTaskPriority(string task)
+        {
+            if (!TaskExist(task))
+                throw new InvalidOperationException($"Task {task} doesn`t exist. ");
+            Tasks.First(x => x.Title == task).Priority = null;
+        }
+        public void AddTask(Task task)
+        {
+            if (!IsValid(task))
+                throw new InvalidOperationException("Task isn`t valid. ");
+            if (TaskExist(task.Title))
+                throw new InvalidOperationException($"Task {task.Title} already exists. ");
+
+            Tasks.Add(task);
+        }
+        public void RemoveTask(string task)
+        {
+            if (!TaskExist(task))
+                throw new InvalidOperationException($"Task {task} doesn`t exists. ");
+            Tasks.Remove(Tasks.First(x => x.Title == task));
+        }
+        public void AddTaskToFavorite(string task)
+        {
+            if (!TaskExist(task))
+                throw new InvalidOperationException($"Task {task} doesn`t exists. ");
+            Tasks.First(x => x.Title == task).Favorite = true;
+        }
+        public void RemoveTaskFromFavorite(string taskTitle)
+        {
+            if (!TaskExist(taskTitle))
+                throw new InvalidOperationException($"Task {taskTitle} doesn`t exists. ");
+            Tasks.First(x => x.Title == taskTitle).Favorite = false;
+        }
+
+        public List<Task> GetFavoriteTasks()
+        {
+            return Tasks.Where(x => x.Favorite == true).OrderBy(x => x.Title).ToList();
+        }
+
+        public List<Task> GetTasksByPriority(string priority)
+        {
+            if (!CheckPriority(priority))
+                throw new InvalidOperationException($"Wrong Priority {priority}. ");
+
+            return Tasks.Where(x => x.Priority == priority).OrderBy(x => x.Title).ToList();
+        }
+
+        public void GetAllTasks()
+        {
+            int i = 0;
+            while (i < Tasks.Count)
+            {
+                ShowTask(i);
+                i++;
+            }
+        }
+    }
+}
---
+++ lab4/TaskConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Lab3TaskManager;
+
+Console.WriteLine("TaskManager is starting");
+var taskManager = new TaskManagement();
+var task = new Lab3TaskManager.Task();
+var task2 = new Lab3TaskManager.Task("Test", "Test", "Low", false);
+taskManager.AddTask(task);
+taskManager.AddTask(task2);
+
+Console.WriteLine("Adding tasks to taskmanager:");
+taskManager.GetAllTasks();
+
+taskManager.AddTaskPriority(task.Title, "High");
+Console.WriteLine($"Change Prioriry for task: {task.Title}");
+taskManager.ShowTask(0);
+
+taskManager.RemoveTaskPriority(task.Title);
+Console.WriteLine($"Remove Prioriry for task: {task.Title}");
+taskManager.ShowTask(0);
+
+taskManager.AddTaskToFavorite(task.Title);
+Console.WriteLine($"Adding task {task.Title} to Favorite ");
+taskManager.ShowTask(0);
+
+taskManager.RemoveTaskFromFavorite(task.Title);
+Console.WriteLine($"Removing task {task.Title} from Favorite ");
+taskManager.ShowTask(0);
+
+taskManager.RemoveTask(task.Title);
+Console.WriteLine($"Removing task {task.Title} from List:");
+taskManager.GetAllTasks();
+
+Console.WriteLine("All Tests Done!");
